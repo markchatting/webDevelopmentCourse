063d37c2727734e242b5085664182d42c7129d5e
markchatting <57613411+markchatting@users.noreply.github.com>
2022-11-10 10:52:54 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Spaghetti</li>
+      <li>Shwarma</li>
+      <li>Steak</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
